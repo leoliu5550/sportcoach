3384d72028ee6d7adb4fd2d143405521a2b5cecf
Leo <leoliu5550@gmail.com>
2022-05-27 23:43:36 +0800

    git add

--- data_storage.py
+++ data_storage.py
@@ -1,11 +1,5 @@
-import pickledb
-# https://patx.github.io/pickledb/commands.html
-
-db = pickledb.load('example.db', True)
-db.set('key', 'value')
-db.set('key2', 'value2')
-db.set('key3', 'value3')
-db.append('key','001')
-print(db.totalkeys())
-
-# db.dump()
+# import pickledb
+# # https://patx.github.io/pickledb/commands.html
+# db = pickledb.load('SPORT.db', True)
+# # db.set('acerobic','')
+# db.dcreate('acerobic')
