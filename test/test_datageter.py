18e54c17bae9daeb88a1e9a132b5ca43d17948b1
Leo <leoliu5550@gmail.com>
2022-07-09 18:50:15 +0800

    update the sport select part

--- test/test_datageter.py
+++ test/test_datageter.py
@@ -15,19 +15,16 @@ class Test_data(object):
             data = json.load(file)
         assert data == data_gt.sport_data()
 
-    def test_sport_geter(self,mocker):
+    def test_status_geter(self,mocker):
         # 對Product中的方法的返回值進行Mock
         mock_value = {"0":["REST","SLOWEAR"]}
         data_gt.status_data = mocker.patch(
             "utils.datageter.sport.status_data",
             return_value=mock_value)
-        #  (str(day),weekDays[day]) 
 
-        sportType = data_gt.sport_geter(("0","Sunday"))
-
+        sportType = data_gt.status_geter(("0","Sunday"))
         assert sportType == ["REST","SLOWEAR"]
-    # def sport_geter(self,date:Tuple) -> list:
-    #     status_data = self.status_data()
-    #     sport_index = status_data[date[0]]
 
-    #     return sports_index
+    def test_sport_geter(self):
+
+        pass
--- utils/datageter.py
+++ utils/datageter.py
@@ -1,25 +1,35 @@
 import json
 from typing import Any,Dict,Tuple
+import random
 
 class sport(object):
     
-    def __init__(self) -> None:
+    def __init__(self,status_path = '',sport_path = '') -> None:
+        if status_path != '':
+            self.status_path = status_path
+        else:
+            self.status_path = 'data//STATUS.json'
+
+        if sport_path != '':
+            self.sport_path = sport_path
+        else:
+            self.sport_path = 'data//SPORT.json'
         pass
 
-    def status_data(self,path='') -> Dict[str, list]:
-        if path == '':
-            path = 'data//STATUS.json'
+    def status_data(self) -> Dict[str, list]:
+        path = self.status_path
+
         with open(path,'r') as file:
             data = json.load(file)
         return data
 
-    def sport_data(self,path='') -> Any:
-        if path =='':
-            path = 'data//SPORT.json'
+    def sport_data(self) -> Any:
+        path = self.sport_path
+
         with open(path,'r') as file:
             data = json.load(file)
         return data    
-
+    
     #  should get timer (str(day),weekDays[day]) 
     def status_geter(self,data:Tuple) -> list:
         status_data = self.status_data()
@@ -27,11 +37,45 @@ class sport(object):
         sportType = status_data[sport_index]
         return sportType 
 
-    def sport_geter(self):
+    # Aerobic PART
+    def aerobic(self) -> str:
+        sportbase = sport_data()["SPORT"]
+        
+        if data[1] == "SLOWEAR":
+            sportbase = sportbase["Aerobic"]["SLOWEAR"]
+            
+            pass
+        else: #  data[1] == "HIIT"
+            sportbase = sportbase["Aerobic"]["HIIT"]
+            pass
+        sportbase = list(sportbase.keys())
+        result = random.choice(sportbase)
+
+        return result
+
+    # Weight PART
+    def Weight(self,category:int) -> str:
+        '''
+        category:
+            0 : "Strength"  set 8
+            1 : "Endurance" set 20
+        '''
+        sportbase = sport_data()["SPORT"]["Weight"]
+        if category == 0:
+            sportbase = sportbase["Strength"]
+        else:
+            sportbase = sportbase["Endurance"]
+
+        sportbase = list(sportbase.keys())  
+        result = random.choice(sportbase)
+        return result 
 
 
 
+    def sport_geter(self,data:list) -> None:
+
+        sportbase["Weight"]
+        sportbase["Soft"]
 
         pass
 
-    
